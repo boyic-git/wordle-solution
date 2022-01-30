0cd4e45debf3a9f5a108c6a142eed95d1490b4ba
BoyiChen <21008254+boyic-git@users.noreply.github.com>
2022-01-29 23:16:10 -0800

    first commit, loadwords and getFrequency/Probs

---
+++ getFrequency.py
@@ -0,0 +1,46 @@
+import collections
+from loadWords import load_words
+from collections import Counter
+from math import log
+
+def getFreq():
+    allWords = load_words()
+    freq = Counter()
+    count = 0
+    words = []
+
+    # count how many 5-letter words and 
+    # total frequency of letters of all 5-letter words
+    for w in allWords:
+        if len(w) == 5:
+            freq += Counter(w)
+            count += 1
+            words.append(w)
+
+    words.sort()
+    return count, words, dict(freq.most_common(26))
+    # print(count, freq.most_common(26))
+
+## get probability of all words as a dict
+## or get probability of a specific word
+def getProb(word=None):
+    count, words, freqs = getFreq()
+    for letter in freqs:
+        freqs[letter] = freqs[letter]/count/5
+    probs = {}
+    for w in words:
+        temp = 0
+        for c in w:
+            temp += log(freqs[c])
+        probs[w] = temp
+
+    if word and word in probs:
+        return probs[word]
+    elif word and word not in probs:
+        print("{} is probably not a word".format(word))
+    return probs
+
+
+if __name__ == "__main__":
+    count, w, freq = getFreq()
+    print(getProb("hello"))
